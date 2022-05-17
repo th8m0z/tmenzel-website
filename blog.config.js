c46dd74e65b2eaa26a5f2770bf36972c6f1d255f
th8m0z <thomas.mz8@icloud.com>
2022-05-17 20:26:34 +0200

    stuff

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'Neo',
+  title: 'Thomas Menzel',
   author: 'Thomas Menzel',
-  email: 'i@craigary.net',
+  email: 'thomas@proddy.app',
   link: 'thomasmz.com',
   description: 'Geldwechselproblem lösen',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/matrixnotes',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
